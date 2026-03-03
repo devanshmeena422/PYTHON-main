9c5a1d60950f43371ffebc26783b012d27d36e72
Devansh Meena <devanshmeena422@gmail.com>
2026-03-03 21:55:14 +0530

    new file

---
+++ 13_vote_eligibility.py
@@ -0,0 +1,7 @@
+age = float(input("Enter your age : "))
+if(age>18):
+    print("You are eligible to vote")
+elif(age==18):
+    print("show your age proof")
+else:
+    print("You are not eligible for vote")
