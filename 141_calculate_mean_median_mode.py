d17fac1e2437f360b4f9b596dc62ea076efd6af8
Devansh Meena <devanshmeena422@gmail.com>
2026-04-14 23:07:11 +0530

    new  file

---
+++ 141_calculate_mean_median_mode.py
@@ -0,0 +1,11 @@
+import statistics
+
+data = [10, 20, 20, 30, 40]
+
+mean = statistics.mean(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+
+print("Mean:", mean)
+print("Median:", median)
+print("Mode:", mode)
